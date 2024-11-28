ed59e72044ddb23a741d423bf9579800b8ef1482
eric-qli <92590396+eric-qli@users.noreply.github.com>
2024-11-28 17:45:33 -0500

    added e11

---
+++ pages/csc309-e11.js
@@ -0,0 +1,8 @@
+export default function Csc309E11Page() {
+    return (
+      <div id="auto-tester">
+        Hi. I am 1007654307, and I have learned how to develop a full-stack web app
+        and deploy it!
+      </div>
+    );
+  }
